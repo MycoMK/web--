915d47fdf280f15259e66716fefd7c43e818b015
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-11-09 21:36:04 -0800

    js-edits

---
+++ app.js
@@ -0,0 +1,21 @@
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+//  buttons
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+// counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translateX("+ (-size*counter)+ "px)";
+
+// button listeners
+nextBtn.addEventListener("click",()=>{
+carouselSlide.style.transition = "transform 0.4s ease-in-out";
+counter++;
+carouselSlide.style.transform = "translateX("+ (-size*counter)+ "px)";
+
+ });
+
