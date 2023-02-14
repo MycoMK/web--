e8d43f0a7a64f5e64dca007d26cebf1b8e00ee29
Myco <kmichael831@gmail.com>
2023-02-14 12:57:47 -0800

    edits

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 
+let btn1 = document.querySelector("#btn-1")
+let btn2 = document.querySelector("#btn-2")
+let btn3 = document.querySelector("#btn-3")
+let btn4 = document.querySelector("#btn-4")
+let btn5 = document.querySelector("#btn-5")
 
+
+ btn1.addEventListener('click',() => {
+    document.body.style.backgroundImage = `url('1.jpeg')`;
+ })
+
+
+ btn1.addEventListener('click',() => {
+    document.body.style.backgroundImage = `url('2.jpeg')`;
+ })
+
+ btn1.addEventListener('click',() => {
+    document.body.style.backgroundImage = `url('3.jpeg')`;
+ })
+
+ btn1.addEventListener('click',() => {
+    document.body.style.backgroundImage = `url('4.jpeg')`;
+ })
+
+ btn1.addEventListener('click',() => {
+    document.body.style.backgroundImage = `url('5.jpeg')`;
+ })
